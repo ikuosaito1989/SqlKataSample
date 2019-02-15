1364628596747f312fd4e121ec3035896ababa78
斉藤育男 <ikuo@saitouikuonoMacBook-ea.local>
2019-02-15 21:00:31 +0900

    First Commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using SqlKata;
+using SqlKata.Compilers;
+using SqlKata.Execution;
+using System.Data.SqlClient;
+
+namespace SqlKataSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection("");
+            var compiler = new SqlServerCompiler();
+            var db = new QueryFactory();
+            var query = db.Query(nameof(Hoge))
+                .Join(nameof(Piyo), "Hoge.PiyoId", "Piyo.Id")
+                .OrderBy("Hoge.Id");
+            var sql = compiler.Compile(query).Sql;
+        }
+    }
+    public class Hoge {
+        public int Id { get; set; }
+        public int PiyoId { get; set; }
+    }
+
+    public class Piyo {
+        public int Id { get; set; }
+    }
+    
+}
